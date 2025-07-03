8e9a4ea1c95fd2c049cbdbb7c508b221e8ed46d7
Prayag2006 <prayagkansara05@gmail.com>
2025-07-03 18:29:06 +0530

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,21 +1,24 @@
-alert("First open a console log in your tab.")
-//Hotel menu
-console.log("======================MENU=========================");
-console.log(" No.          Food           Price");
-console.log("1            PIZZA            850");
-console.log("2            PASTA            450");
-console.log("3           BURGER            150");
-console.log("4            MAGGIE            50");
-console.log("5            COFFEE           150");
-console.log("6         COLD COFFEE         200");
-
+//Project of hotel menu
 let totalBill = 0;
 let option;
 //Do while loop started
 do {
+  //hotel menu
+  alert(
+    "===========MENU=========\n" +
+    " No.          Food           Price\n" +
+    "1            PIZZA            850\n" +
+    "2            PASTA            450\n" +
+    "3           BURGER            150\n" +
+    "3           BURGER            150\n"+
+    "4            MAGGIE            50\n"+
+    "5            COFFEE           150\n"+
+    "6         COLD COFFEE         200"
+  )
+//User choice 
   let choice = Number(prompt("Enter your choice: "));
   let quantity, price = 0;
-
+//switch case started
   switch (choice) {
     case 1:
       console.log("PIZZA");
@@ -51,15 +54,12 @@ do {
       console.log("Invalid choice!");
       price = 0;
   }
-
-  console.log("Subtotal: ₹" + price);
-  totalBill += price;
-
+  //switch case started
+  alert(("Subtotal: ₹" + price))
+  console.log(totalBill += price)
   option = Number(prompt("Do you want to order something else? Enter 1 for Yes, 0 for No: "));
 
 } while (option === 1);
-//Do while loop end
-console.log("============ THANK YOU FOR YOUR ORDER ============");
-console.log("================== VISIT AGAIN ====================");
-console.log(`YOUR TOTAL BILL : ₹${totalBill}`);
-
+//Do while loop end here
+alert("==THANK YOU FOR YOUR ORDER==\n"+"======== VISIT AGAIN ========\n"+`YOUR TOTAL BILL : ₹${totalBill}`)
+//Project end here
